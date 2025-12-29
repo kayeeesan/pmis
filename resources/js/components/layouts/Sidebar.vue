1ae77801b67f138adf9b972060e120af56a70cb7
kayeeesan <eijansantosdominique13@gmail.com>
2025-12-29 18:32:56 +0800

    sidebar enhance

--- resources/js/components/layouts/Sidebar.vue
+++ resources/js/components/layouts/Sidebar.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, watch } from "vue";
+import { ref, watch, reactive } from "vue";
 
 const props = defineProps({
     drawer: {
@@ -11,6 +11,12 @@ const props = defineProps({
 const items = [
     { title: 'Home', icon: 'mdi-home', route: "/" },
 ];
+
+// collapsible section states
+const groups = reactive({
+  libraries: true,
+  files: false,
+});
 
 const libraries = [
     { title: 'Role', route: "/roles" },
@@ -30,67 +36,117 @@ const files = [
 ];
 
 const open = ref(true);
-const isRail = ref(true);
+const mode = reactive({
+  rail: true,
+  temporary: false,
+  floating: true,
+  app: false,
+});
 
 watch(
     () => props.drawer,
     (value)  => {
-        // When drawer is active (true), expand to full width (rail=false)
-        // When inactive (false), collapse to rail (mini variant)
-        isRail.value = !value;
-    }
+        if (value) {
+            // Open: full drawer, part of layout (content shifts)
+            open.value = true;
+            mode.rail = false;
+            mode.floating = false;
+            mode.app = true;
+            mode.temporary = false;
+        } else {
+            // Closed: keep a mini rail, floating over content (no layout shift)
+            open.value = true; // keep visible to show rail
+            mode.rail = true;
+            mode.floating = true;
+            mode.app = false;
+            mode.temporary = false;
+        }
+    },
+    { immediate: true }
 );
 </script>
 <template>
-    <v-navigation-drawer v-model="open" :rail="isRail" :permanent="true" app>
-        <v-list>
-            <template v-for="item in items">
-                <v-list-item
-                    :prepend-icon="item.icon"
-                    :title="item.title"
-                    :to="item.route"
-                ></v-list-item>
-            </template>
+  <v-navigation-drawer
+    v-model="open"
+    :rail="mode.rail"
+    :temporary="mode.temporary"
+    :permanent="!mode.temporary"
+    :floating="mode.floating"
+    :app="mode.app"
+    :expand-on-hover="mode.rail"
+    width="260"
+    elevation="6"
+    border="end"
+    theme="dark"
+    class="sidebar-gradient"
+  >
+    <v-sheet class="pa-4 d-flex align-center" color="transparent">
+      <v-avatar color="cyan-darken-3" size="40" class="mr-3">
+        <v-icon icon="mdi-cube-outline"></v-icon>
+      </v-avatar>
+      <div class="font-weight-medium text-white">PMIS</div>
+    </v-sheet>
+    <v-divider></v-divider>
 
-            <v-list-group value="Actions">
-                <template v-slot:activator="{ props }">
-                    <v-list-item
-                        prepend-icon="mdi-format-list-bulleted-type"
-                        v-bind="props"
-                        title="Libraries"
-                    ></v-list-item>
-                </template>
+    <v-list density="comfortable" nav>
+      <v-list-item
+        v-for="item in items"
+        :key="item.title"
+        :prepend-icon="item.icon"
+        :title="item.title"
+        :to="item.route"
+        rounded
+        active-color="cyan-accent-2"
+      ></v-list-item>
 
-                <v-list-item
-                    v-for="library in libraries"
-                    :title="library.title"
-                    :to="library.route"
-                ></v-list-item>
-            </v-list-group>
+      <v-divider class="my-2" color="white" opacity="0.12"></v-divider>
 
-            <v-list-group value="Files">
-                <template v-slot:activator="{ props }">
-                    <v-list-item
-                    prepend-icon="mdi-folder-multiple"
-                    v-bind="props"
-                    title="Files"
-                    ></v-list-item>
-                </template>
+      <v-list-group v-model="groups.libraries" value="libraries">
+      <template #activator="{ props }">
+        <v-list-item
+          v-bind="props"
+          prepend-icon="mdi-format-list-bulleted-type"
+          title="Libraries"
+          class="text-white"
+        />
+      </template>
+      <v-list-item
+        v-for="library in libraries"
+        :key="library.title"
+        :title="library.title"
+        :to="library.route"
+        rounded
+        active-color="cyan-accent-2"
+      />
+    </v-list-group>
 
-                <v-list-item
-                    v-for="file in files"
-                    :key="file.title"
-                    :title="file.title"
-                ></v-list-item>
-            </v-list-group>
+    <v-divider class="my-2" color="white" opacity="0.12"></v-divider>
 
-        </v-list>
-      </v-navigation-drawer>
+    <v-list-group v-model="groups.files" value="files">
+      <template #activator="{ props }">
+        <v-list-item
+          v-bind="props"
+          prepend-icon="mdi-folder-multiple"
+          title="Files"
+          class="text-white"
+        />
+      </template>
+      <v-list-item
+        v-for="file in files"
+        :key="file.title"
+        prepend-icon="mdi-folder-outline"
+        :title="file.title"
+        rounded
+        active-color="cyan-accent-2"
+      />
+    </v-list-group>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 <style>
-.v-navigation-drawer--rail.v-navigation-drawer--expand-on-hover:not(.v-navigation-drawer--is-hovering) .v-list-item .v-avatar, .v-navigation-drawer--rail:not(.v-navigation-drawer--expand-on-hover) .v-list-item .v-avatar{
-    width: 38px;
-    height: 38px;
+.sidebar-gradient {
+  background: linear-gradient(180deg, #0f172a, #111827);
+  color: #fff;
 }
 .nav-header {
     margin-top: 10px
@@ -99,4 +155,10 @@ watch(
     font-size: 15px;
     font-weight: bold;
 }
+
+.v-navigation-drawer.v-navigation-drawer--rail {
+  width: 68px !important;
+}
+
+
 </style>
